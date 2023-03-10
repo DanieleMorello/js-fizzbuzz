c50b7f120cd3451ceb55dfb1afb4875887b1c0cb
DanieleMorello <danielemorello@live.it>
2023-03-10 18:52:51 +0100

    correct bug of backtick

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,12 +26,13 @@ console.log(divElement);
 
 // Scrivi un programma che stampi in console i numeri da 1 a 100(ciclo for)
 for(let i = 1; i < 101; i++) {
-  const squareEl = divElement.innerHTML += '<div class="square d-flex align-items-center justify-content-center m-3">`${i}`</div>';
+  const squareEl = divElement.innerHTML += `<div class="square d-flex align-items-center justify-content-center m-3">${i}</div>`;
   // per i numeri che sono sia multipli di 3 che di 5 stampi 
   // “FizzBuzz”.
   if  (i % 3 === 0 && i % 5 === 0) {
     console.log('FizzBuzz');
   // per i multipli di 3 stampi “Fizz” al posto del numero
+    squareEl.innerHTML = 'FizzBuzz';
   } else if (i % 3 === 0) {
     
     console.log('Fizz');
