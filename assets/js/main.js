38cbe14d8c8c6fcf13b207ac0c5a55cc497650f7
DanieleMorello <danielemorello@live.it>
2023-03-10 15:35:52 +0100

    Add for loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,20 @@ far svolgere al nostro programma così come lo faremmo
 -3. Facciamo attenzione all'ordine delle condizioni che usiamo
 */
 
+// Scrivi un programma che stampi in console i numeri da 1 a 100(ciclo for)
+for(let i = 1; i < 101; i++) {
+  
+  // per i numeri che sono sia multipli di 3 che di 5 stampi 
+  // “FizzBuzz”.
+  if (i % 3 === 0) {
+    console.log('Fizz');
+    // i multipli di 3 stampi “Buzz”
+  } else if (i % 5 === 0) {
+    console.log('Buzz');
+    // per i multipli di 5 stampi “Buzz”
+  } else if (i % 3 === 0 && i % 5 === 0) {
+    console.log(i,'FizzBuzz');
+   } else {
+     console.log(i);
+   }
+}
