e8510b08835a53e87e77bab23c43fe53f88ffe76
DanieleMorello <danielemorello@live.it>
2023-03-10 17:34:56 +0100

    change order of condition

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,21 +26,19 @@ console.log(divElement);
 
 // Scrivi un programma che stampi in console i numeri da 1 a 100(ciclo for)
 for(let i = 1; i < 101; i++) {
-  
+  const squareEl = divElement.innerHTML += '<div class="square d-flex align-items-center justify-content-center m-3">`${i}`</div>';
   // per i numeri che sono sia multipli di 3 che di 5 stampi 
   // “FizzBuzz”.
-  if (i % 3 === 0) {
-    const squareEl = divElement.innerHTML += '<div class="square d-flex align-items-center justify-content-center m-3">`${i}`</div>';
+  if  (i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz');
+  // per i multipli di 3 stampi “Fizz” al posto del numero
+  } else if (i % 3 === 0) {
+    
     console.log('Fizz');
     
-    // i multipli di 3 stampi “Buzz”
+    // per i multipli di 5 stampi “Buzz”
   } else if (i % 5 === 0) {
-    console.log('Buzz');
-    
-    // per i multipli di 5 stampi “FizzBuzz”
-  } else if (i % 3 === 0 && i % 5 === 0) {
-    console.log('FizzBuzz');
-    
+    console.log('Buzz'); 
     
    } else {
      console.log(i);
